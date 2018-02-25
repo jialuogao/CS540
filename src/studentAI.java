9e1e09d23f873f2197ec21dce79c661075ed650f
Unknown <jialuogao980513@gmail.com>
2018-02-25 13:10:52 -0600

    finalversion

--- src/studentAI.java
+++ src/studentAI.java
@@ -13,14 +13,14 @@ public class studentAI extends Player {
     public int alphabetaSearch(BoardState state, int maxDepth) {
     	int bestMove=0;
     	int maxValue=0;
-    	int currentDepth = 0;
+    	int currentDepth = maxDepth;
     	int alpha = Integer.MIN_VALUE;
     	int beta = Integer.MAX_VALUE;
     	maxValue = maxValue(state,maxDepth,currentDepth,alpha,beta);
     	for(int i=0;i<6;i++) {
     		if(maxForMoves[i]==maxValue) {
     			if(state.isLegalMove(1, i)) {
-    				return i;    				
+    				return i;
     			}
     		}
     	}
@@ -30,7 +30,7 @@ public class studentAI extends Player {
     
     public int maxValue(BoardState state, int maxDepth, int currentDepth, int alpha, int beta) {
     	int player = 1;
-    	if(currentDepth==maxDepth || state.status(2)!=BoardState.GAME_NOT_OVER) {
+    	if(currentDepth==0 || state.status(player)!=BoardState.GAME_NOT_OVER) {
     		return sbe(state);
     	}
     	else {
@@ -39,8 +39,8 @@ public class studentAI extends Player {
     		for(int i=0;i<6;i++) {
     			if(state.isLegalMove(player, i)) {
     				boardPrediction=state.applyMove(player, i);			
-    				maxValue = Math.max( maxValue , minValue(boardPrediction,maxDepth,currentDepth+1,alpha,beta));
-    				if(currentDepth==0) {
+    				maxValue = Math.max( maxValue , minValue(boardPrediction,maxDepth,currentDepth-1,alpha,beta));
+    				if(currentDepth==maxDepth) {
     					maxForMoves[i]=maxValue;
     				}
     				if(maxValue>=beta) {
@@ -55,7 +55,7 @@ public class studentAI extends Player {
 
     public int minValue(BoardState state, int maxDepth, int currentDepth, int alpha, int beta) {
     	int player = 2;
-    	if(currentDepth==maxDepth || state.status(1)!=BoardState.GAME_NOT_OVER) {
+    	if(currentDepth==0 || state.status(player)!=BoardState.GAME_NOT_OVER) {
     		return sbe(state);
     	}
     	else {
@@ -64,7 +64,7 @@ public class studentAI extends Player {
     		for(int i=0;i<6;i++) {
     			if(state.isLegalMove(player, i)) {
     				boardPrediction=state.applyMove(player, i);			
-    				minValue = Math.min( minValue , maxValue(boardPrediction,maxDepth,currentDepth+1,alpha,beta));
+    				minValue = Math.min( minValue , maxValue(boardPrediction,maxDepth,currentDepth-1,alpha,beta));
     				if(minValue<=alpha) {
     					return minValue;
     				}
